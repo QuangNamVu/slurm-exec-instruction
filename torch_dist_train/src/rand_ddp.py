685c4d2742a8ee74435fb50e77d44c0d2e43bcaf
NamVQ <vqnam97@gmail.com.vn>
2024-05-27 20:02:44 +0700

    slurm DDP for random

--- torch_dist_train/src/rand_ddp.py
+++ torch_dist_train/src/rand_ddp.py
@@ -19,10 +19,8 @@ class MyTrainDataset(Dataset):
     def __getitem__(self, index):
         return self.data[index]
 
-def ddp_setup():
-    init_process_group(backend="nccl")
-    # init_process_group(backend="gloo")
-    # init_process_group(backend="mpi")
+def ddp_setup(backend="nccl"):
+    init_process_group(backend=backend)
     print("using backend NCCL")
     print("="*20)
     print(os.environ["LOCAL_RANK"])
@@ -112,14 +110,14 @@ def prepare_dataloader(dataset: Dataset, batch_size: int):
     )
 
 
-def main(save_every: int, total_epochs: int, batch_size: int, delete_checkpoint=False, snapshot_path: str = "snapshot.pt"):
+def main(save_every: int, total_epochs: int, batch_size: int, backend="gloo", delete_checkpoint=False, snapshot_path: str = "snapshot.pt"):
     if delete_checkpoint:
         print("delete snapshot file")
         try:
             os.remove(snapshot_path)
         except OSError:
             pass
-    ddp_setup()
+    ddp_setup(backend=backend)
     dataset, model, optimizer = load_train_objs(batch_size=batch_size)
     train_data = prepare_dataloader(dataset, batch_size)
     trainer = Trainer(model, train_data, optimizer, save_every, snapshot_path)
@@ -130,9 +128,10 @@ def main(save_every: int, total_epochs: int, batch_size: int, delete_checkpoint=
 if __name__ == "__main__":
     print("starting main")
     parser = argparse.ArgumentParser(description='simple distributed training job')
+    parser.add_argument('--backend', default="gloo", type=str, help='backend for torch distributed')
     parser.add_argument('--delete_checkpoint', default=True, type=int, help='delete checkpoint')
     parser.add_argument('--batch_size', default=2048, type=int, help='Input batch size on each device (default: 32)')
     parser.add_argument('--total_epochs', default=100, type=int, help='Total epochs to train the model')
     parser.add_argument('--save_every', default=100, type=int, help='How often to save a snapshot')
     args = parser.parse_args()
-    main(args.save_every, args.total_epochs, args.batch_size, delete_checkpoint=args.delete_checkpoint, snapshot_path="/mnt/rand_snapshot.pt")
+    main(args.save_every, args.total_epochs, args.batch_size, backend=args.backend, delete_checkpoint=args.delete_checkpoint, snapshot_path="/mnt/rand_snapshot.pt")
